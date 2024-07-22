dfd6ce0ffa1737fe69afce58122cd7c6f756a9da
rufeng <rufeng@zbyte-inc.com>
2024-07-22 14:33:56 +0800

    feat: project frame

---
+++ chatbot-star-application/src/main/java/com/rufeng/application/ChatBotStarApplication.java
@@ -0,0 +1,11 @@
+package com.rufeng.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatBotStarApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ChatBotStarApplication.class, args);
+    }
+}
---
+++ chatbot-star-interfaces/src/main/java/com/rufeng/interfaces/ChatBotStarInterfacesApplication.java
@@ -0,0 +1,11 @@
+package com.rufeng.interfaces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatBotStarInterfacesApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ChatBotStarInterfacesApplication.class, args);
+    }
+}
